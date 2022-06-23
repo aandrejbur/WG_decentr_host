3857e55171cf2753a97b3ac12c4fa8b1608b0f76
Andrei Burankou <aandrejbur@gmail.com>
2022-06-23 21:49:44 +0300

    Initial Commit

---
+++ WG_decentr_host/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  WG_decentr_host
+//
+//  Created by Андрей Буренков on 23.06.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
